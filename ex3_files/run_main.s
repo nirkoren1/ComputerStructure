74662d89af3c65103a5ebf2684715d6aada0ecce
Nir Koren <nirkoren1@gmail.com>
2022-12-18 15:37:09 +0200

    .

--- ex3_files/run_main.s
+++ ex3_files/run_main.s
@@ -11,12 +11,15 @@ format_i_print: .string "%d\n"                     # format for scanning integer
 format_s_print: .string "%s\n"                     # format for scanning strings
 format_c_print: .string "%c"                       # format for printing characters
     .section    .data
-size_1: .int 0                                     # size of the first string
-size_2: .int 0                                     # size of the second string
+size_1: .byte 0                                     # size of the first string
+size_2: .byte 0                                     # size of the second string
 option: .int 0                                     # option in the menu
     .section    .bss
 string_1: .space 100                               # first string
 string_2: .space 100                               # second string
+pstring_1: .space 256                                 # pointer to the first string
+pstring_2: .space 256                                 # pointer to the second string
+
     .text
     .globl run_main
     .type run_main, @function
@@ -29,56 +32,53 @@ run_main:
     leaq size_1(%rip), %rsi                        # load size_1 to rsi
     movq $0, %rax                                  # load 0 to rax
     call scanf                                     # call scanf
+    movzbq size_1(%rip), %r8                        # load size_1 to r8
 
                                                    # get the first string
     leaq format_s_scan(%rip), %rdi                 # load format_s to rdi
     leaq string_1(%rip), %rsi                      # load string_1 to rsi
     movq $0, %rax                                  # load 0 to rax
     call scanf                                     # call scanf
-
-    #print the first string by every character
-    leaq string_1(%rip), %rdi                      # load string_1 to rdi
-    .loop1:                                        # loop for printing the first string
-    movb (%rdi), %al                               # load the character to al
-    cmpb $0, %al                                   # compare the character to 0
-    je .end1                                       # if the character is 0 jump to .end1
-    leaq format_c_print(%rip), %rsi                # load format_c to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call printf                                    # call printf
-    incq %rdi                                      # increment rdi
-    jmp .loop1                                     # jump to .loop1
-
+    movq string_1(%rip), %r9                       # load string_1 to r9
 
                                                    # get the size of the second string
     leaq format_i_scan(%rip), %rdi                 # load format_i to rdi
     leaq size_2(%rip), %rsi                        # load size_2 to rsi
     movq $0, %rax                                  # load 0 to rax
     call scanf                                     # call scanf
+    movzbq size_2(%rip), %r10                        # load size_2 to r10
 
                                                    # get the second string
     leaq format_s_scan(%rip), %rdi                 # load format_s to rdi
     leaq string_2(%rip), %rsi                      # load string_2 to rsi
     movq $0, %rax                                  # load 0 to rax
     call scanf                                     # call scanf
+    movq string_2(%rip), %r11                      # load string_2 to r11
 
                                                    # build the first struct
-    leaq string_1(%rip), %rdi                      # load string_1 to rdi
-    leaq size_1(%rip), %rsi                        # load size_1 to rsi
+    movq %r8, %rdi                                 # load size_1 to rdi
+    movq %r9, %rsi                                 # load string_1 to rsi
     movq $0, %rax                                  # load 0 to rax
     call build_struct                              # call build_struct
-    pushq %rax                                     # push the address of the first struct to the stack
-
 
                                                    # build the second struct
-    leaq string_2(%rip), %rdi                      # load string_2 to rdi
-    leaq size_2(%rip), %rsi                        # load size_2 to rsi
+    movq %r10, %rdi                                # load size_2 to rdi
+    movq %r11, %rsi                                # load string_2 to rsi
     movq $0, %rax                                  # load 0 to rax
     call build_struct                              # call build_struct
-    pusq %rax                                      # save the address of the second struct
+
+                                                      # print pstring_1
+    leaq format_i_print(%rip), %rdi                # load format_i to rdi
+    leaq pstring_1(%rip), %rsi                     # load pstring_1 to rsi
+    movb (%rsi), %sil                               # load pstring_1 to sil
+    movq $0, %rax                                  # load 0 to rax
+    call printf                                    # call printf
+
 
     movq $0, %rax
     leave
     ret
+
 
     .text
     .globl build_struct
@@ -87,19 +87,18 @@ build_struct:
     pushq %rbp                                     # save the base pointer
     movq %rsp, %rbp                                # set the base pointer
 
-                                                   # get the string from the user
-    movq %rsi, (%rax)                              # save the string length in the struct
-    movq $0, %rsi                                  # load 0 to rdi
+                                                   # load the size of the struct to pstring first byte
+    movq (%rsi), %rax                              # load the size of the struct to rax
+    movq %rax, pstring_1(%rip)                      # load rax to pstring_1
 
-    leaq string_1(%rip), %rdi                      # load string_1 to rdi
-    .loop1:                                        # loop for printing the first string
-    movb (%rdi), %al                               # load the character to al
-    leaq %al,
-    cmpb $0, %al                                   # compare the character to 0
-    je .end1                                       # if the character is 0 jump to .end1
-    incq %rdi                                      # increment rdi
-    jmp .loop1                                     # jump to .loop1
+                                                   # loop every character in the string and save it in the struct pstring
+    .loop:
+    movzbl (%rdi), %eax                            # load the first character of the string to rax
+    movb %al, pstring_1(%rip)                       # load rax to pstring_1
+    addq $1, %rdi                                  # add 1 to rdi
+    addq $1, %rax                                  # add 1 to rax
+    cmpq %rax, %rsi                                # compare rax to rsi
+    jne .loop                                      # jump to .loop if rax != rsi
 
-    movq $0, %rax
     leave
     ret
