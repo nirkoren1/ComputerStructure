34362788a8d3d076108f768fc14eee0fcde3de5d
nirkoren1 <nirkoren1@gmail.com>
2022-12-19 00:49:52 +0200

    run_main.s finished

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nirkoren/ComputerStructure
+# Install script for directory: /home/nirkoren/university/ComputerStructure
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nirkoren/ComputerStructure/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nirkoren/university/ComputerStructure/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- ex3_files/run_main.s
+++ ex3_files/run_main.s
@@ -7,18 +7,14 @@
     .section    .rodata
 format_i_scan: .string "%d\0"                      # format for scanning integers
 format_s_scan: .string "%s\0"                      # format for scanning strings
-format_i_print: .string "%d\n"                     # format for scanning integers
-format_s_print: .string "%s\n"                     # format for scanning strings
-format_c_print: .string "%c"                       # format for printing characters
     .section    .data
-size_1: .byte 0                                     # size of the first string
-size_2: .byte 0                                     # size of the second string
-option: .int 0                                     # option in the menu
-    .section    .bss
-string_1: .space 100                               # first string
-string_2: .space 100                               # second string
-pstring_1: .space 256                                 # pointer to the first string
-pstring_2: .space 256                                 # pointer to the second string
+    .align 1
+size_1: .byte 0                                    # size of the first string
+string_1: .space 256                               # pointer to the first struct
+size_2: .byte 0                                    # size of the second string
+string_2: .space 256                               # pointer to the second struct# option in the menu
+option: .int 0
+
 
     .text
     .globl run_main
@@ -28,77 +24,47 @@ run_main:
     movq %rsp, %rbp                                # set the base pointer
 
                                                    # get the size of the first string
-    leaq format_i_scan(%rip), %rdi                      # load format_i to rdi
-    leaq size_1(%rip), %rsi                        # load size_1 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call scanf                                     # call scanf
-    movzbq size_1(%rip), %r8                        # load size_1 to r8
+    leaq size_1(%rip), %rsi                        # set the first argument for scanf
+    leaq format_i_scan(%rip), %rdi                 # set the second argument for scanf
+    movq $0, %rax                                  # set the return value to 0
+    call scanf                                     # call scanf to scan the size of the first string
 
                                                    # get the first string
-    leaq format_s_scan(%rip), %rdi                 # load format_s to rdi
-    leaq string_1(%rip), %rsi                      # load string_1 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call scanf                                     # call scanf
-    movq string_1(%rip), %r9                       # load string_1 to r9
+    leaq string_1(%rip), %rsi                      # set the first argument for scanf
+    leaq format_s_scan(%rip), %rdi                 # set the second argument for scanf
+    movq $0, %rax                                  # set the return value to 0
+    call scanf                                     # call scanf to scan the first string
 
                                                    # get the size of the second string
-    leaq format_i_scan(%rip), %rdi                 # load format_i to rdi
-    leaq size_2(%rip), %rsi                        # load size_2 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call scanf                                     # call scanf
-    movzbq size_2(%rip), %r10                        # load size_2 to r10
+    leaq size_2(%rip), %rsi                        # set the first argument for scanf
+    leaq format_i_scan(%rip), %rdi                 # set the second argument for scanf
+    movq $0, %rax                                  # set the return value to 0
+    call scanf                                     # call scanf to scan the size of the second string
 
                                                    # get the second string
-    leaq format_s_scan(%rip), %rdi                 # load format_s to rdi
-    leaq string_2(%rip), %rsi                      # load string_2 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call scanf                                     # call scanf
-    movq string_2(%rip), %r11                      # load string_2 to r11
+    leaq string_2(%rip), %rsi                      # set the first argument for scanf
+    leaq format_s_scan(%rip), %rdi                 # set the second argument for scanf
+    movq $0, %rax                                  # set the return value to 0
+    call scanf                                     # call scanf to scan the second string
 
-                                                   # build the first struct
-    movq %r8, %rdi                                 # load size_1 to rdi
-    movq %r9, %rsi                                 # load string_1 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call build_struct                              # call build_struct
+                                                   # get the option from the menu
+    leaq option(%rip), %rsi                        # set the first argument for scanf
+    leaq format_i_scan(%rip), %rdi                 # set the second argument for scanf
+    movq $0, %rax                                  # set the return value to 0
+    call scanf                                     # call scanf to scan the option from the menu
 
-                                                   # build the second struct
-    movq %r10, %rdi                                # load size_2 to rdi
-    movq %r11, %rsi                                # load string_2 to rsi
-    movq $0, %rax                                  # load 0 to rax
-    call build_struct                              # call build_struct
+    movzb size_1(%rip), %rdx                       # set the rdx register to the address of the size of the first string
+    leaq format_i_print(%rip), %rdi                # set the rsi register to the address of the format for printing integers
+    movq %rdx, %rsi                                # set the rdi register to the address of the size of the first string
+    movq $0, %rax                                  # set the return value to 0
+    call printf                                    # call printf to print the size of the first string
 
-                                                      # print pstring_1
-    leaq format_i_print(%rip), %rdi                # load format_i to rdi
-    leaq pstring_1(%rip), %rsi                     # load pstring_1 to rsi
-    movb (%rsi), %sil                               # load pstring_1 to sil
-    movq $0, %rax                                  # load 0 to rax
-    call printf                                    # call printf
+    leaq size_1(%rip), %rsi                        # set the first argument for run_func
+    leaq size_2(%rip), %rdi                        # set the second argument for run_func
+    movq option(%rip), %rdx                        # set the third argument for run_func
+    movq $0, %rax                                  # set the return value to 0
+    call run_func                                  # call run_func
 
+    leave                                          # restore the base pointer
+    ret                                            # return from the function
 
-    movq $0, %rax
-    leave
-    ret
-
-
-    .text
-    .globl build_struct
-    .type build_struct, @function
-build_struct:
-    pushq %rbp                                     # save the base pointer
-    movq %rsp, %rbp                                # set the base pointer
-
-                                                   # load the size of the struct to pstring first byte
-    movq (%rsi), %rax                              # load the size of the struct to rax
-    movq %rax, pstring_1(%rip)                      # load rax to pstring_1
-
-                                                   # loop every character in the string and save it in the struct pstring
-    .loop:
-    movzbl (%rdi), %eax                            # load the first character of the string to rax
-    movb %al, pstring_1(%rip)                       # load rax to pstring_1
-    addq $1, %rdi                                  # add 1 to rdi
-    addq $1, %rax                                  # add 1 to rax
-    cmpq %rax, %rsi                                # compare rax to rsi
-    jne .loop                                      # jump to .loop if rax != rsi
-
-    leave
-    ret
